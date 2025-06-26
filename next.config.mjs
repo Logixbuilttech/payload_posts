37e2b0f62900bfef1232db46b47488bca9bcf46e
logix <logix@gmail.com>
2025-06-26 18:19:49 +0530

    fix : image isssue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Specify the protocol (e.g., 'https')
+        hostname: 'payload-vercel-website-demo.vercel.app', // Replace with the actual hostname of your external image
+        // Optional: Add a pathname if you want to restrict to specific paths
+        // pathname: '/path/to/images/**', 
+      },
+      // Add more objects for other external domains if needed
+    ],
+  },
   experimental: {
     reactCompiler: false,
   },
